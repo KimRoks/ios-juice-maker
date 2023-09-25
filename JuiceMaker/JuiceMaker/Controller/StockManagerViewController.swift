94f36f3da126b45f80df8b968f07412f649c15e4
KimRoks <rudfhr3161@naver.com>
2023-09-25 16:08:17 +0900

    feat: configure properties
    - StockManagerViewController 의 Label 타입 정의

--- JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 final class StockManagerViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    @IBOutlet private var strawberryQuantityLabel: StrawberryQuantityLabel!
+    @IBOutlet private var bananaQuantityLabel: BananaQuantityLabel!
+    @IBOutlet private var pineappleQuantityLabel: PineappleQuantityLabel!
+    @IBOutlet private var kiwiQuantityLabel: KiwiQuantityLabel!
+    @IBOutlet private var mangoQuantityLabel: MangoQuantityLabel!
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
